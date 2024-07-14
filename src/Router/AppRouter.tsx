d8daee85268b18de6a650df602806375299fa24a
Aditya Singh <aditya.singh@binmile.com>
2024-07-14 22:40:06 +0530

    made profile-page

--- src/Router/AppRouter.tsx
+++ src/Router/AppRouter.tsx
@@ -1,11 +1,16 @@
-// src/router/AppRouter.tsx
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Here from '../pages/Landing/Hero';
 import Dashboard from '../pages/Dashboard/Dashboard';
 import Register from '../pages/User/Register';
 import Login from '../pages/User/Login';
+import ProfilePage from '../pages/Profile/Profile';
 
-
+const user = {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    profilePicture: "https://via.placeholder.com/150",
+  };
+  
 const AppRouter = () => (
     <Router>
         <Routes>
@@ -17,6 +22,7 @@ const AppRouter = () => (
             <Route path="/register" element={<Register />} /> */}
             {/* Add more routes as needed */}
             <Route path="/signup" element={<Register />} /> 
+            <Route path="/profile-page" element={<ProfilePage user={user}/>}/>
            <Route path="/login" element={<Login />} />
         </Routes>
     </Router>
