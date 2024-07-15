c08df5fde26530e0604d1c0f2fbe581d1e5cbf5d
Aditya Singh <aditya.singh@binmile.com>
2024-07-15 10:09:04 +0530

    added loader and profile-page v1

---
+++ src/components/common/Loader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../styles/Loader.css'
+const Loader: React.FC = () => {
+  return (
+    <div className="loader">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Loader;
